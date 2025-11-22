3346839f1d8b500328749b2531c8bf926f3425dd
sayan <beinghrick@gmail.com>
2025-11-23 02:49:06 +0530

    middleware commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@
 
 import dotenv from "dotenv";
 import connectToDatabase from "./db/index.js";
+import app from "./app.js";
 dotenv.config({ path: "./.env" });
 
-connectToDatabase();
+connectToDatabase().then(
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    }
+    )
+).catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+});
 
 
 
